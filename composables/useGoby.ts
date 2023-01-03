9e4fce3ce227dfaa66d0b3d3f21067e78a9c4090
Andreas Greimel <andreas@mintgarden.io>
2023-01-03 21:38:48 +0100

    Add basic types to goby composable

--- composables/useGoby.ts
+++ composables/useGoby.ts
@@ -1,3 +1,5 @@
+import { Ref } from '@vue/reactivity';
+
 const isGobyInstalled = () => {
   if (typeof window === 'undefined') {
     return false;
@@ -6,14 +8,14 @@ const isGobyInstalled = () => {
   return Boolean(chia && chia.isGoby);
 };
 
-export const requestAccounts = async (goby) => {
+export const requestAccounts = async (goby: Ref) => {
   if (goby.value.isGobyInstalled) {
     const accounts = await (window as any).chia.request({ method: 'requestAccounts' });
     goby.value.account = accounts?.[0];
   }
 };
 
-export const takeOffer = async (goby, offer) => {
+export const takeOffer = async (goby: Ref, offer: { offer: string }) => {
   if (goby.value.isGobyInstalled) {
     try {
       await (window as any).chia.request({ method: 'takeOffer', params: { offer: offer.offer, fee: 0 } });
@@ -27,14 +29,18 @@ export const useGoby = () => {
   if (!goby.value.isGobyInstalled && isGobyInstalled()) {
     goby.value.isGobyInstalled = true;
 
-    (window as any).chia.on('accountsChanged', (accounts) => {
+    (window as any).chia.on('accountsChanged', (accounts: any[] | undefined) => {
       goby.value.account = accounts?.[0];
     });
     (window as any).chia.on('chainChanged', () => window.location.reload());
-    (window as any).chia.request({ method: 'accounts' }).then((accounts) => {
+    (window as any).chia.request({ method: 'accounts' }).then((accounts: any[] | undefined) => {
       goby.value.account = accounts?.[0];
     });
   }
 
-  return { goby, requestAccounts: () => requestAccounts(goby), takeOffer: (offer) => takeOffer(goby, offer) };
+  return {
+    goby,
+    requestAccounts: () => requestAccounts(goby),
+    takeOffer: (offer: { offer: string }) => takeOffer(goby, offer),
+  };
 };
