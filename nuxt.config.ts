c6d661f2b7c71574a6ec9df50fb66c58eda4ab8d
Andreas Greimel <andreas@mintgarden.io>
2023-01-03 17:23:54 +0100

    Add meta info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindForms from '@tailwindcss/forms'
-import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
-
+import tailwindForms from '@tailwindcss/forms';
+import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineNuxtConfig({
   ssr: false,
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'nostr-dex',
+      meta: [
+        {
+          name: 'description',
+          content: 'An experimental trustless decentralized exchange using the nostr protocol and Chia offers.',
+        },
+      ],
+    },
+  },
   tailwindcss: {
     config: {
-      plugins: [tailwindForms]
-    }
+      plugins: [tailwindForms],
+    },
   },
   vite: {
     resolve: {
       alias: {
         stream: 'stream-browserify',
-      }
+      },
     },
     optimizeDeps: {
       esbuildOptions: {
         define: {
           global: 'globalThis',
         },
-        plugins: [
-          NodeGlobalsPolyfillPlugin()
-        ],
+        plugins: [NodeGlobalsPolyfillPlugin()],
       },
     },
   },
-})
+});
