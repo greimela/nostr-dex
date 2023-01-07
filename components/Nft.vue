861af473dcb3c4cbc4cb7406a0bcc75f4a489c06
Andreas Greimel <andreas@mintgarden.io>
2023-01-07 13:04:56 +0100

    Center NFT image

--- components/Nft.vue
+++ components/Nft.vue
@@ -5,7 +5,7 @@ const { nft } = defineProps({
 </script>
 <template>
   <NuxtLink target="_blank" :to="`https://mintgarden.io/nfts/${nft.id}`">
-    <img class="h-48 w-48 object-cover" :src="nft.thumbnail_uri" />
+    <img class="mx-auto h-48 w-48 object-cover" :src="nft.thumbnail_uri" />
     {{ nft.name }}
   </NuxtLink>
 </template>
