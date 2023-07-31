052138e7ab46dbd1c56e892a9805b367d78dcdfa
Andreas Greimel <andreas@mintgarden.io>
2023-07-31 15:02:56 +0200

    Show name for CATs in TailDatabase

--- components/Cat.vue
+++ components/Cat.vue
@@ -5,10 +5,12 @@ const { amount, cat } = defineProps({
   amount: [BigNumber, Number],
   cat: Object,
 });
+
+const { data } = useFetch(`https://mainnet-api.taildatabase.com/tail/${cat.tailHash}`);
 </script>
 <template>
   <NuxtLink target="_blank" :to="`https://www.taildatabase.com/tail/${cat.tailHash}`" class="inline-flex gap-1">
     <img class="h-6 w-6" :src="`https://icons.dexie.space/${cat.tailHash}.webp`" />{{ util.formatToken(amount) }}
-    {{ cat.symbol }}
+    {{ data?.name || "UNKNOWN" }}
   </NuxtLink>
 </template>
